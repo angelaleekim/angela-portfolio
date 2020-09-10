f0493880dab6963bbe08dde69cb175114b5b7619
Angela Kim <angelakim@Angelas-MacBook-Pro-3.local>
2020-09-10 13:57:43 -0700

    Updates

---
+++ precache-manifest.6c551c1b263207b9823ed13cebd21345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af44e88d3e3e1c0008c107e44c36db7b",
+    "url": "/angela-portfolio/index.html"
+  },
+  {
+    "revision": "975c7ef04ac7275070ab",
+    "url": "/angela-portfolio/static/css/main.d6d3fbda.chunk.css"
+  },
+  {
+    "revision": "1e071a79d4222bd8a737",
+    "url": "/angela-portfolio/static/js/2.d9c78662.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/angela-portfolio/static/js/2.d9c78662.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "975c7ef04ac7275070ab",
+    "url": "/angela-portfolio/static/js/main.50a294a0.chunk.js"
+  },
+  {
+    "revision": "8b4be06eac15bd357cef",
+    "url": "/angela-portfolio/static/js/runtime-main.b959a596.js"
+  }
+]);
